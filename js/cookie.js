3740ca36d5425af3c04ae91afa75d9b921fde54c
woodySGE <woody.hunter@simplygreat.education>
2026-06-09 14:27:58 +0100

    the working one?

--- js/cookie.js
+++ js/cookie.js
@@ -7,4 +7,20 @@ function makeCookie() {
     if (input) {
         document.cookie(input)
     }
+
+
 }
+
+
+    
+    
+ //       function makeCookie() {
+   //         //word = document.cookie(value)
+     //       var input = document.getElementsById("chosenword")[0];
+//
+ //           if (input) {
+  //              //let thing = document.getElementsById("test")[0];
+   //             //thing = input
+    //            document.cookie(input)
+     //       }
+      //  }
